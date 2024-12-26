c23eaf7d2fd9dcfeb3f448aa8af90b3ca496df35
Maxx <maxxssj338@gmail.com>
2024-12-26 21:16:29 +0530

    js

--- script.js
+++ script.js
@@ -29,17 +29,17 @@ if(x>600){
   document.getElementById("nav-clicon").style.display="none";
 }
 }
-function  openMenu(){
-  document.getElementById("nav").style.display="block";
-  document.getElementById("body").style.overflow="hidden";
-  document.getElementById("nav-opicon").style.display="none";
-  document.getElementById("nav-clicon").style.display="block";
-  // document.getElementById("wel-card").style.display="none";
-}
-function  closeMenu(){
-  document.getElementById("body").style.overflow="auto";
-  document.getElementById("nav").style.display="none";
-  document.getElementById("nav-opicon").style.display="block";
-  document.getElementById("nav-clicon").style.display="none";
-  // document.getElementById("wel-card").style.display="block";
-}
+// function  openMenu(){
+//   document.getElementById("nav").style.display="block";
+//   document.getElementById("body").style.overflow="hidden";
+//   document.getElementById("nav-opicon").style.display="none";
+//   document.getElementById("nav-clicon").style.display="block";
+//   // document.getElementById("wel-card").style.display="none";
+// }
+// function  closeMenu(){
+//   document.getElementById("body").style.overflow="auto";
+//   document.getElementById("nav").style.display="none";
+//   document.getElementById("nav-opicon").style.display="block";
+//   document.getElementById("nav-clicon").style.display="none";
+//   // document.getElementById("wel-card").style.display="block";
+// }
